04e36180e87797809b573e7a8ac5935af306fb1d
Josh Adams <jdadams1@loyola.edu>
2024-09-25 09:06:48 -0400

    Halloween added!

---
+++ src/Halloween.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("My favorite holloween costume was when I dressed up as Bumblebee from Transformers! - Josh");
+    }
+}
