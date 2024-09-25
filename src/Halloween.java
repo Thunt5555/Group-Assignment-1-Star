3f74f6041d2ff413d9807196160092625730a0f6
jakehoogstrate <jakehoogstrate5@gmail.com>
2024-09-25 13:40:45 -0400

    added halloween costume

--- src/Halloween.java
+++ src/Halloween.java
@@ -3,5 +3,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("My favorite Halloween Costume is the headless Horseman");
+
+        System.out.printf("My favorite Halloween Costume is a monkey");
     }
 }
