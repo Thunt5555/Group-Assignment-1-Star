4fc7e8ee4a455ddab43f398d1475c21658839001
jakehoogstrate <jakehoogstrate5@gmail.com>
2024-11-20 01:23:51 -0500

    made firebaseConfig.js

---
+++ Bullshit/firebaseConfig.js
@@ -0,0 +1,14 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB44xbxuYHv3_VjpQ1zcuiKHkOesal50xM",
+    authDomain: "bsregistration.firebaseapp.com",
+    projectId: "bsregistration",
+    storageBucket: "bsregistration.appspot.com",
+    messagingSenderId: "203675511936",
+    appId: "1:203675511936:web:c7f469220ea8402f580a65",
+    measurementId: "G-2DMHJZMW1F"
+};
+
+
+export default firebaseConfig;
